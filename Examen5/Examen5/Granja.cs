87795110da0d2ea0e40664dd7a49ef4ce5fd4dfe
Peep17 <113267056+Peep17@users.noreply.github.com>
2023-10-02 23:05:05 -0500

    CrearClaseGranja

---
+++ Examen5/Examen5/Granja.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen5
+{
+
+    class Planta
+    {
+        public string nombre;
+        public int TiempoVida;
+        public int FrutasVerduras;
+        public float ValorSemilla;
+        public float ValorProducto;
+
+    }
+
+    class Animal
+    {
+        public string nombre;
+        public int TiempoVida;
+        public int CantidadCarne;
+        public int CantidadLeche;
+        public int CantidadHuevos;
+        public int CantidadLana;
+        public float PrecioAnimal;
+        public float PrecioFinales;
+    }
+    class Granja
+    {
+        private List<Planta> listaPlantas;
+        private List<Animal> listaAnimales;
+
+        public Granja()
+        {
+            listaPlantas = new List<Planta>();
+            listaAnimales = new List<Animal>();
+        }
+
+        public void AgregarPlanta(Planta planta)
+        {
+            listaPlantas.Add(planta);
+        }
+
+        public void AgregarAnimal(Animal animal)
+        {
+            listaAnimales.Add(animal);
+        }
+
+        public List<Planta> ListaPlantas()
+        {
+            return listaPlantas;
+        }
+
+        public List<Animal> ListaAnimales()
+        {
+            return listaAnimales;
+        }
+    }
+}
